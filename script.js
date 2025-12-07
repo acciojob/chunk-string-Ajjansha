efb6764ffa0a40a4e4125ab5e71ac7e50440055a
Ajjansha <141652880+Ajjansha@users.noreply.github.com>
2025-12-07 21:56:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function stringChop(str, size) {
-  // your code here
+function stringChop(str, chunkLength) {
+  // If string is null → return empty array
+  if (str === null) return [];
+
+  const result = [];
+
+  // Convert chunkLength to number (prompt returns string)
+  chunkLength = Number(chunkLength);
+
+  for (let i = 0; i < str.length; i += chunkLength) {
+    result.push(str.substring(i, i + chunkLength));
+  }
+
+  return result;
 }
 
 // Do not change the code below
